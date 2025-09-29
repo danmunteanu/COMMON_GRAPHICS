86e7d7a7dddd67b9f11936d0f02327cb4d3b84b6
Dan Munteanu <epsdel@gmail.com>
2025-09-29 10:47:37 +0300

    Moved render algoritm to separate class in CommonGraphics

---
+++ Renderer.cs
@@ -0,0 +1,65 @@
+﻿
+namespace CommonGraphics
+{
+    public class Renderer
+    {
+        public delegate void ProgressCallback(int percent);
+        public delegate void ChunkRenderer(Point start, Point end, byte[] pixels, int stride);
+
+        public Bitmap? Buffer { get; set; } = null;
+
+        protected int _plottedPixels = 0;
+        protected int _pixelsToPlotCount = 0;
+        protected int _lastReportedPercent = 0;
+
+        //  Callback for update
+        public ProgressCallback? Callback { get; set; } = null;
+        public ChunkRenderer? RenderTask { get; set; } = null;
+
+        public virtual void Render()
+        {
+            if (Buffer == null)
+                return;
+
+            if (RenderTask == null)
+                return;
+
+            //  Split render area in multiple chunks
+            List<(Point start, Point end)>? chunks = null;
+            Chunks.CreateRenderChunks(Buffer.Width, Buffer.Height, out chunks);
+
+            //  Something went wrong while chunking
+            if (chunks == null)
+                return;
+
+            // Lock the bitmap once
+            var rect = new Rectangle(0, 0, Buffer.Width, Buffer.Height);
+            var bmpData = Buffer.LockBits(rect,
+                System.Drawing.Imaging.ImageLockMode.WriteOnly,
+                System.Drawing.Imaging.PixelFormat.Format32bppArgb
+            );
+
+            IntPtr ptr = bmpData.Scan0;
+            int stride = bmpData.Stride;
+            byte[] pixels = new byte[stride * Buffer.Height];
+
+            _plottedPixels = 0;
+            _pixelsToPlotCount = Buffer.Width * Buffer.Height;
+            _lastReportedPercent = 0;
+            Callback?.Invoke(0);
+
+            // Render all chunks in parallel
+            Parallel.ForEach(chunks, chunk =>
+            {
+                RenderTask(chunk.start, chunk.end, pixels, stride);
+            });
+
+            // Copy back into bitmap
+            System.Runtime.InteropServices.Marshal.Copy(pixels, 0, ptr, pixels.Length);
+            Buffer.UnlockBits(bmpData);
+
+            // 100% progress
+            Callback?.Invoke(100);
+        }
+    }
+}
