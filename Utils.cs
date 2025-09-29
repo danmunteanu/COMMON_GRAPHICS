5ee360954fccc510c2cba8b5f26a5b57d12e21dd
Dan Munteanu <epsdel@gmail.com>
2025-09-29 11:22:31 +0300

    Caching fractals

---
+++ Utils.cs
@@ -0,0 +1,21 @@
+﻿namespace CommonGraphics
+{
+    public class Utils
+    {
+        public static bool AddUserControlToPanel(Panel panel, UserControl control)
+        {
+            if (panel.Controls.Contains(control))
+                return false;
+
+            if (control == null)
+                return false;
+
+            control.Dock = DockStyle.Fill;
+            panel.Controls.Clear();
+            panel.Controls.Add(control);
+            control.BringToFront();
+
+            return true;
+        }
+    }
+}
