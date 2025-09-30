9e863caa4248be831ed92deaeafb3cd0f0d458f4
Dan Munteanu <epsdel@gmail.com>
2025-09-30 23:16:57 +0300

    Good stuff.

--- Utils.cs
+++ Utils.cs
@@ -17,5 +17,31 @@
 
             return true;
         }
+
+        public static void NumericUpDown_SelectAll(object? sender, EventArgs e)
+        {
+            if (sender is NumericUpDown nud)
+            {
+                // Get the internal TextBox
+                var tb = nud.Controls[1] as TextBox;
+                tb?.SelectAll();
+            }
+        }
+
+        public static void NumericUpDown_MouseUp(object? sender, MouseEventArgs e)
+        {
+            if (sender is NumericUpDown nud)
+            {
+                var tb = nud.Controls[1] as TextBox;
+                if (tb != null && !tb.Focused)
+                    return;
+
+                // Select all text only if no selection exists
+                if (tb != null && tb.SelectionLength == 0)
+                {
+                    tb.SelectAll();
+                }
+            }
+        }
     }
 }
